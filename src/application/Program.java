9e6852f10f1b3ef13823f0efcf45336315b5e6bc
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-07-28 18:34:54 -0300

    create seller entity

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import entities.Department;
+import entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department obj = new Department(1, "TI");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj);
+		System.out.println(seller);
 	}
 
 }
--- src/entities/Department.java
+++ src/entities/Department.java
@@ -6,7 +6,7 @@ public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
-	private int id;
+	private Integer id;
 	private String name;
 	
 	public Department() {}
