9dae0f3dda2536015b93c8bec9cc520cc45a3f00
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-07-29 18:05:38 -0300

    sellerdao and factory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 	}
 
 }
