735765b61ac50caea8b412beef06b2f8a358ff70
Thalis Duarte <thalis_galeno@hotmail.com.br>
2020-07-30 16:44:28 -0300

    findById

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,17 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department obj = new Department(1, "TI");
-		System.out.println(obj);
-		
-		Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 2000.0, obj);
-		System.out.println(seller);
-		
+					
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 	}
 
 }
